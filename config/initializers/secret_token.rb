6866512fb26ee254f3491f8b2cb6c6ea67b9623b
vijayajyothi <jyothis.kalangi@gmail.com>
2015-05-25 22:54:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinuxEndpoint::Application.config.secret_token = '6c3c3d0279d0e598cd17b31f8ae9cc250b7a16704eea10f56c7629bc786592d4e406e1300a5d8891d07eac04810efb467a8562091012d503a893e081a18930e8'
