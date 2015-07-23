67493dd4c69abef41e9b994b7cf2636f7b2f8835
vijayajyothi <jyothis.kalangi@gmail.com>
2015-07-23 03:28:10 -0700

    change

--- app/mailers/mbs_mailer.rb
+++ app/mailers/mbs_mailer.rb
@@ -4,6 +4,6 @@ class MbsMailer < ActionMailer::Base
    def confirmation_mail(request)
     @vmrequest = request
 #    mail(to: "#{@vmrequest.requester}, #{@vmrequest.other}", subject:"New VM Build request")
-  mail(to: "#{@vmrequest.requester}, #{@vmrequest.other}, itops-vmbuilt@vmware.com", subject:"New VM Build request")
+  mail(to: "#{@vmrequest.requester}, #{@vmrequest.other}, itops-vmbuild@vmware.com", subject:"New VM Build request")
   end
 end
