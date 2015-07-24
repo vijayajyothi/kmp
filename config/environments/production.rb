6088b43fde89aa9bd3afa34af78879c9aaa31f69
vijayajyothi <jyothis.kalangi@gmail.com>
2015-07-24 03:40:52 -0700

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ LinuxEndpoint::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+    config.assets.debug = true
+config.assets.enabled = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
