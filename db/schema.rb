e8967f50a84da85681a824d9dba42506ab267ed1
jyothi <kalangiv@vmware.com>
2017-09-17 23:23:55 -0700

    doc if loop

--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,6 @@ ActiveRecord::Schema.define(:version => 20170918052118) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "resumes", :force => true do |t|
-    t.string   "name"
-    t.string   "attachment"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "vmrequests", :force => true do |t|
     t.string   "name"
     t.string   "environment"
